e3a458853132395202ff51b137ccf6d3ab793280
LaythAlqadhi <125077045+LaythAlqadhi@users.noreply.github.com>
2024-01-22 19:52:29 +0000

    Change the HomePage component name into AuthPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 import Layout from './components/Layout';
-import { useAuth } from './contexts/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
 
 function App() {
-  const { token } = useAuth();
-
   return (
     <Routes>
       <Route path="/" element={<Layout />} />
-      <Route index element={<HomePage />} />
+      <Route index element={<AuthPage />} />
       <Route path="auth/signin" element={<SignInPage />} />
       <Route path="auth/signup" element={<SignUpPage />} />
     </Routes>
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
-function HomePage() {
+function AuthPage() {
   const navigate = useNavigate();
-  
+
   const handleContinueWithGitHub = () => {
     window.location.href = 'https://b32a7bae-6556-4da3-a848-f0e0b80bf4f0-00-36mr5e3zsor9c.janeway.replit.dev/v1/auth/github';
   }
@@ -16,4 +16,5 @@ function HomePage() {
     </div>
   );
 }
-export default HomePage;
+
+export default AuthPage;
