e666588a16897f269c1a6bee6f5eb88402be09e4
LaythAlqadhi <125077045+LaythAlqadhi@users.noreply.github.com>
2024-02-13 23:54:01 +0000

    Redesign and add share functionality to UserProfileHeader

--- src/components/UserProfileHeader.jsx
+++ src/components/UserProfileHeader.jsx
@@ -1,21 +1,18 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { mdiPlusCircle } from '@mdi/js';
 import useFetch from '../hooks/useFetch';
 import OwnerProfileButtons from './OwnerProfileButtons';
 import VisitorProfileButtons from './VisitorProfileButtons';
-
-const API_URL =
-  'https://b32a7bae-6556-4da3-a848-f0e0b80bf4f0-00-36mr5e3zsor9c.janeway.replit.dev/v1';
+import Icon from './Icon';
+import { API_URL } from '../constants';
 
 function UserProfileHeader({ userId, token, me }) {
   const { fetchData, data, loading, error } = useFetch();
 
   useEffect(() => {
-    fetchData(`${API_URL}/user/${userId}`, token);
+    fetchData(`${API_URL}/users/${userId}`, token);
   }, [API_URL, userId, token]);
-
-  const isOwner = me && userId && me.id === userId;
 
   if (loading) return <div>Loading...</div>;
 
@@ -23,41 +20,69 @@ function UserProfileHeader({ userId, token, me }) {
 
   if (data && data.user) {
     const { user } = data;
+    const isOwner = me && userId && me.id === userId;
 
     return (
-      <div>
-        <div>
-          <div>
-            <Link to={`/profile/${user.id}`}>
-              <img src={user.profile.avatar} alt="Avatar" />
-            </Link>
-            <span>+</span>
-          </div>
-          <div>
-            <button type="button">
-              <span>{user.posts.length}</span>
-              <span>Posts</span>
-            </button>
-            <button type="button">
-              <span>{user.followers.length}</span>
-              <span>Followers</span>
-            </button>
-            <button type="button">
-              <span>{user.following.length}</span>
-              <span>Following</span>
-            </button>
+      <div className="grid-rows-[repeat(3,_1fr] grid grid-cols-3 gap-4 p-4">
+        <div className="col-start-1 row-start-1 sm:row-end-4">
+          <div className="relative w-fit">
+            <img
+              className="w-32 rounded-full sm:w-40 lg:w-48 xl:w-56 2xl:w-64"
+              src={user.profile.avatar}
+              alt="Avatar"
+            />
+            {isOwner && (
+              <button
+                className="absolute bottom-0 right-0 rounded-full bg-white"
+                type="button"
+                aria-label="Update your avatar"
+              >
+                <Icon
+                  className="icon h-8 w-8 fill-sky-500 hover:fill-sky-600 sm:h-10 sm:w-10 lg:h-12 lg:w-12 xl:h-12 xl:w-12"
+                  path={mdiPlusCircle}
+                />
+              </button>
+            )}
           </div>
         </div>
-        <div>
-          <span aria-label="Name">{user.profile.displayName}</span>
-          <span aria-label="Username">{user.username}</span>
-          {user.profile.bio && <span aria-label="Bio">{user.profile.bio}</span>}
+        <div className="col-span-2 row-start-1 flex items-center justify-center gap-4 sm:col-span-1">
+          <button className="flex flex-col items-center" type="button">
+            <span className="text-2xl font-semibold">{user.posts.length}</span>
+            <span className="text-lg">Posts</span>
+          </button>
+          <button className="flex flex-col items-center" type="button">
+            <span className="text-2xl font-semibold">
+              {user.followers.length}
+            </span>
+            <span className="text-lg">Followers</span>
+          </button>
+          <button className="flex flex-col items-center" type="button">
+            <span className="text-2xl font-semibold">
+              {user.following.length}
+            </span>
+            <span className="text-lg">Following</span>
+          </button>
         </div>
-        {isOwner ? (
-          <OwnerProfileButtons />
-        ) : (
-          <VisitorProfileButtons token={token} me={me} user={user} />
-        )}
+        <div className="col-span-3 row-start-2 sm:col-span-1">
+          <div className="flex flex-col p-2">
+            <span className="text-lg font-semibold" aria-label="Name">
+              {user.profile.displayName}
+            </span>
+            <span aria-label="Username">@{user.username}</span>
+            {user.profile.bio && (
+              <span className="text-base" aria-label="Bio">
+                {user.profile.bio}
+              </span>
+            )}
+          </div>
+        </div>
+        <div className="col-span-3 row-start-3 sm:col-span-1">
+          {isOwner ? (
+            <OwnerProfileButtons user={user} />
+          ) : (
+            <VisitorProfileButtons token={token} me={me} user={user} />
+          )}
+        </div>
       </div>
     );
   }
