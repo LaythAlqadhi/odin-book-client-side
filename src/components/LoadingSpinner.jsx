527be31a9317bef303b8f9fd040c37a927684f37
LaythAlqadhi <125077045+LaythAlqadhi@users.noreply.github.com>
2024-02-13 23:22:05 +0000

    Add LoadingSpinner component

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -4,9 +4,9 @@ function LoadingSpinner() {
   return (
     <div
       data-testid="loading"
-      className="flex h-screen w-full items-center justify-center"
+      className="flex h-screen w-full cursor-wait items-center justify-center"
     >
-      <div className="h-20 w-20 animate-spin rounded-full border-8 border-gray-200 border-t-blue-500" />
+      <div className="h-20 w-20 animate-spin rounded-full border-8 border-t-sky-500" />
     </div>
   );
 }
