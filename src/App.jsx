0ac14ed7f2f99574364bd69f71586dfbe51ee744
LaythAlqadhi <125077045+LaythAlqadhi@users.noreply.github.com>
2024-02-13 23:40:57 +0000

    Update Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
@@ -7,42 +8,62 @@ import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 import ProfilePage from './pages/ProfilePage';
 import FollowRequestsPage from './pages/FollowRequestsPage';
-import ProtectedRoute from './components/ProtectedRoute';
+import SearchPage from './pages/SearchPage';
+import PostPage from './pages/PostPage';
+import NotFoundPage from './pages/NotFoundPage';
 import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />} />
-      <Route
-        index
-        element={
-          <ProtectedRoute redirectPath="/auth">
-            <HomePage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/auth"
-        element={
-          <ProtectedRoute redirectPath="/" isAuthenticated={false}>
-            <AuthPage />
-          </ProtectedRoute>
-        }
-      >
-        <Route path="signin" element={<SignInPage />} />
-        <Route path="signup" element={<SignUpPage />} />
-      </Route>
-      <Route path="/profile/:userId" element={<ProfilePage />} />
-      <Route
-        path="/notification/follow-requests"
-        element={
-          <ProtectedRoute redirectPath="/auth">
-            <FollowRequestsPage />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route
+          index
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/auth"
+          element={
+            <ProtectedRoute redirectPath="/" isAuthenticated={false}>
+              <AuthPage />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="signin" element={<SignInPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+        </Route>
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+        <Route
+          path="/notification/follow-requests"
+          element={
+            <ProtectedRoute>
+              <FollowRequestsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <ProtectedRoute>
+              <SearchPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/post/:postId"
+          element={
+            <ProtectedRoute>
+              <PostPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
