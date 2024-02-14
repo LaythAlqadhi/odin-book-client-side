717711d9b177382a64896b60eabaed6322a78066
LaythAlqadhi <125077045+LaythAlqadhi@users.noreply.github.com>
2024-02-14 01:31:56 +0000

    Back to the previous API_URL

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 /* eslint-disable import/prefer-default-export */
 export const API_URL =
-  'https://odinbook-api.adaptable.app/v1';
+  'https://b32a7bae-6556-4da3-a848-f0e0b80bf4f0-00-36mr5e3zsor9c.janeway.replit.dev/v1';
